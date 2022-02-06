cddd15144c784c2e8e81f17ee52e3b1995dcd69b
Ragnar Bråstein <ragnarob@protonmail.com>
2022-02-06 02:41:59 +0100

    Make /utstillinger/:id page

--- src/routes/utstillinger/[id].json.js
+++ src/routes/utstillinger/[id].json.js
@@ -1,7 +1,30 @@
+import { getPointsByNumDogs, getPointsByResult } from '../../scores-and-points';
 import { api } from '../_api';
 
 export const get = async (event) => {
   // event.locals.userid comes from src/hooks.js
   const response = await api('GET', `contests/${event.params.id}`);
-  return response;
+
+  if (response.status >= 400) {
+    return response;
+  }
+
+  const pointsByNumDogs = getPointsByNumDogs(response.body.contest.numberOfDogs);
+
+  const mappedResults = response.body.results.map(resultObj => ({
+    ...resultObj,
+    pointsByAward: getPointsByResult(resultObj.result),
+    pointsByNumDogs,
+  }));
+
+  return {
+    status: 200,
+    body: {
+      ...response.body.contest,
+      pointsByNumDogs,
+      results: mappedResults.sort((r1, r2) =>
+        r1.pointsByAward + r1.pointsByNumDogs > r2.pointsByAward + r2.pointsByNumDogs ? -1 : 1
+      ),
+    }
+  };
 };
--- src/routes/utstillinger/[id].svelte
+++ src/routes/utstillinger/[id].svelte
@@ -1,6 +1,7 @@
 <script context="module">
   export const load = async ({ fetch, params }) => {
     const res = await fetch(`/utstillinger/${params.id}.json`);
+
     if (res.ok) {
       const contest = await res.json();
       return {
@@ -9,64 +10,66 @@
     }
 
     return {
-      props: { isError: true },
+      status: res.status,
+      error: res.error,
     };
   };
 </script>
 
 <script>
   export let contest;
-  export let isError;
 </script>
 
-{#if isError}
-  <h1>Utstilling ikke funnet</h1>
-{:else}
-  <h1>{contest.name}</h1>
+<h1>{contest.name}</h1>
 
-  <div class="contest-info">
-    <p style="grid-column: span 2;">{contest.date}, {contest.location}</p>
-    <p>Arrangør:</p>
-    <p>{contest.host}</p>
-    <p>Hunder:</p>
-    <p>{contest.numberOfDogs}</p>
-    <p>Dommer:</p>
-    <p>{contest.judge}</p>
-  </div>
+<div class="contest-info">
+  <p style="grid-column: span 2;">{contest.date}, {contest.location}</p>
+  <p>Arrangør:</p>
+  <p>{contest.host}</p>
+  <p>Hunder:</p>
+  <p>{contest.numberOfDogs}</p>
+  <p>Dommer:</p>
+  <p>{contest.judge}</p>
+</div>
 
-  <h2>Resultater</h2>
-  {#if contest.results?.length}
-    <table>
-      <thead>
+<h2>Resultater</h2>
+{#if contest.results?.length}
+  <p style="margin: 0.5rem 0">
+    {contest.pointsByNumDogs} tilleggspoeng fra antall deltagende hunder
+  </p>
+  <table>
+    <thead>
+      <tr>
+        <th>Hund</th>
+        <th>Resultat</th>
+        <th>Poeng</th>
+        <th>Kritikk</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each contest.results as result}
         <tr>
-          <th>Hund</th>
-          <th>Resultat</th>
-          <th>Poeng</th>
-          <th>Kritikk</th>
+          <td>
+            <a href={`/hunder/${result.dogId}`}>
+              {result.dogName}
+            </a>
+          </td>
+          <td>{result.result}</td>
+          <td>
+            {result.pointsByAward + result.pointsByNumDogs}
+            ({result.pointsByAward}+{result.pointsByNumDogs})
+          </td>
+          <td>
+            {#if result.critiqueLink}
+              <a href={result.critiqueLink} target="_blank">Se kritikk</a>
+            {/if}
+          </td>
         </tr>
-      </thead>
-      <tbody>
-        {#each contest.results as result}
-          <tr>
-            <td>
-              <a href={`/hunder/${result.dogId}`}>
-                {result.dogName}
-              </a>
-            </td>
-            <td>{result.result}</td>
-            <td>{result.points}</td>
-            <td>
-              {#if result.critiqueLink}
-                <a href={result.critiqueLink} target="_blank">Se kritikk</a>
-              {/if}
-            </td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  {:else}
-    Ingen resultater enda
-  {/if}
+      {/each}
+    </tbody>
+  </table>
+{:else}
+  Ingen resultater enda
 {/if}
 
 <style>
