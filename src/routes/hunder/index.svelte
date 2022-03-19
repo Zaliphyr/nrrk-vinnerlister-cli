fa2e9b1dd13ce0f482e7634610320b7ac867a981
ragnarob <ragnarob@protonmail.com>
2022-03-19 02:15:46 +0100

    Add no dogs yet message to dog list

--- src/routes/hunder/index.svelte
+++ src/routes/hunder/index.svelte
@@ -21,27 +21,33 @@
 
 <h1>Alle hunder</h1>
 
-<table>
-  <thead>
-    <tr>
-      <th>Hund</th>
-      <th>Ant. resultater</th>
-      <th>Nyeste resultat</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each dogList as dog}
+{#if dogList.length}
+  <table>
+    <thead>
       <tr>
-        <td>
-          <a href={`/hunder/${dog.id}`}>
-            {dog.name}
-          </a>
-        </td>
-        <td>Kommer</td>
-        <td>Kommer</td>
-        <!-- <td>{dog.numberOfResults}</td>
-        <td>{dog.lastResultDate}</td> -->
+        <th>Hund</th>
+        <th>Ant. resultater</th>
+        <th>Nyeste resultat</th>
       </tr>
-    {/each}
-  </tbody>
-</table>
+    </thead>
+    <tbody>
+      {#each dogList as dog}
+        <tr>
+          <td>
+            <a href={`/hunder/${dog.id}`}>
+              {dog.name}
+            </a>
+          </td>
+          <td>Kommer</td>
+          <td>Kommer</td>
+          <!-- <td>{dog.numberOfResults}</td>
+          <td>{dog.lastResultDate}</td> -->
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+{:else}
+  <p style="margin-top: 1rem;">
+    Ingen hunder enda.
+  </p>
+{/if}
