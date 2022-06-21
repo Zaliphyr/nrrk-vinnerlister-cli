01eb45249b2d72a84584414647b58c5f91c3bbc0
Ragnarob <ragnarob@protonmail.com>
2022-06-21 19:42:52 +0200

    Sort admin utstillinger by ID

--- src/routes/utstillinger/index.json.js
+++ src/routes/utstillinger/index.json.js
@@ -1,7 +1,13 @@
-import { api } from '../_api';
+import { api } from "../_api";
 
 export const get = async () => {
   // event.locals.userid comes from src/hooks.js
-  const response = await api('GET', 'contests');
+  const response = await api("GET", "contests");
+  if (response.status === 200) {
+    return {
+      status: 200,
+      body: response.body.sort((c1, c2) => (c1.id > c2.id ? -1 : 1)),
+    };
+  }
   return response;
 };
